5689e418d21a0b2ab09111295acd053d2caaa258
sejincp <sagela127@gmail.com>
2025-01-09 10:04:01 -0800

    Minimum MVP

--- controllers/dinners.js
+++ controllers/dinners.js
@@ -32,27 +32,37 @@ router.post('/', ensureSignedIn, async (req, res) => {
     res.redirect('/dinners');
   } catch (err) {
     console.log(err);
-    res.redirect('dinners/new');
+    res.redirect('/dinners/new');
   }
 });
 
 // DELETE /dinners/:dinnerId (DELETE functionality / action)
+router.delete('/:dinnerId', ensureSignedIn, async (req, res) => {
+  try {
+    const dinnerId = req.params.dinnerId;
+    const dinner = await Dinner.findByIdAndDelete(dinnerId);
+    res.redirect('/dinners');
+  } catch (err) {
+    console.log(err);
+    res.redirect('/dinners');
+  }
+});
 
 // GET /dinners/dinnerId/edit (EDIT functionality / action)
 router.get('/:dinnerId/edit', ensureSignedIn, async (req, res) => {
-  const dinner = await Dinner.findById(req.params.dinnerId)
+  const dinner = await Dinner.findById(req.params.dinnerId);
   res.render('dinners/edit.ejs', { title: 'Edit Dinner', dinner});
 });
 
 // PUT /dinners/dinnerId (UPDATE functioinality / action)
 router.put('/:dinnerId', ensureSignedIn, async (req, res) => {
+  const dinner = await Dinner.findById(req.params.dinnerId);
   try {
-    const dinner = req.user.dinners.id(req.params.id);
     dinner.set(req.body);
-    await req.user.save();
+    await dinner.save();
     res.redirect(`/dinners/${dinner._id}`);
-  } catch (e) {
-    console.log(e);
+  } catch (err) {
+    console.log(err);
     res.redirect(`/dinners/${dinner._id}`);
   }
 })
